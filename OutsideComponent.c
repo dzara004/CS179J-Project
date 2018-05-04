1dd8e699aec24f658b46cdce8fa0b1742dca35e5
dzara004 <dzara004@ucr.edu>
2018-05-03 21:44:17 -0700

    Updated formalities in comments

--- OutsideComponent.c
+++ OutsideComponent.c
@@ -3,134 +3,141 @@
 #include <avr/interrupt.h>
 #include <avr/delay.h>
 
-//Libraries given in CS120B
+//Libraries given in CS120B/CS122A
 #include "lcd.h"
 #include "timer.h"
 #include "bit.h"
 #include "usart_ATmega1284.h"
 
-unsigned long numPhases;
-unsigned char stepOut[8] = {0x01, 0x03, 0x02, 0x06, 0x04, 0x0C,0x08,0x09};
-unsigned int maxCnt = 0;
-unsigned int cnt = 0;
+//Global variables for stepper motor
+unsigned long phaseCount;
+unsigned char steps[8] = {0x01, 0x03, 0x02, 0x06, 0x04, 0x0C,0x08,0x09};
+unsigned int counter;
+unsigned char forwardCount;
+unsigned char backwardCount;
+unsigned char rotate;
+unsigned char i;
 
-unsigned char forCnt = 0;
-unsigned char bckCnt = 7;
-unsigned char clockwise = 0x01;
+//Global variables for ultrasonic sensor and communication
+unsigned long timer0Counter;
+unsigned char timerOn;
+unsigned long distance;
+unsigned long input;
+unsigned char count;
+unsigned char pick;
 
-enum buttonStates{buttonWait,buttonPush,buttonGo} buttonState;
-enum Direction {forward, backward} direction;
-unsigned char button = 0x02;
-unsigned char i = 0x00;
+void getDistance() {
+	timerOn = 0x01;
+	_delay_us(10);						  //Wait 10 us
+	PORTD = 0x10;						  //Set trig HIGH
+	_delay_us(10);						  //Wait 10 us
+	PORTD = 0x00;						  //Set trig LOW
+	_delay_us(10);						  //Set 10 us
 
-void Tick() {
-	// Transitions for button pushed
-	switch(buttonState) {
-		case buttonWait:
-		if(!button){
-			buttonState = buttonWait;
-		}
-		else if (button == 0x02) {
-				direction = forward;
-				numPhases = (10 / 5.625) * 64;
-				maxCnt = numPhases;
-				buttonState = buttonGo;
-				forCnt = 0;
-				bckCnt = 7;
-				cnt = 0;
+	EICRA |= (1 << ISC11) | (1 << ISC10); //Trigger on rising edge of INT1
+	EIMSK |= 1 << INT1;					  //Enable interrupts on INT1
+	_delay_ms(10);
+
+	EIMSK = 0x00;						  //Disable interrupts on INT1
+	EICRA = 0x00;						  //Reset triggering event
+	timerOn = 0x00;						  //Stop timer variable
+	EICRA |= (1 << ISC11) | (0 << ISC10); //Trigger on falling edge of INT1
+	//EIFR |= 1 << INTF1;				  //Reset flag as required by datasheet
+	EIMSK |= 1 << INT1;					  //Enable interrupts on INT1 again
+	//_delay_ms(10);						  //Delay time to "catch" falling edge, else falling edge doesn't get picked up
+	count = 0;
+}
+
+//States for stepper motor
+enum Motor{wait, rotate} motor;
+enum Rotation {clockwise, counterClockwise} route;
+
+//State machine code for stepper motor
+void MotorTick() {
+	
+	//Transitions
+	switch(motor) {
+		case wait:
+			if (rotate == 0x02) {
+				route = clockwise;
+				phaseCount = (30 / 5.625) * 64;	//Rotate 30 degrees clockwise
+				motor = rotate;
+				forwardCount = 0;
+				backwardCount = 7;
+				counter = 0;
 				++i;
-			} else if (button == 0x01) {
-				direction = backward;
-				numPhases = (10 / 5.625) * 64;
-				maxCnt = numPhases;
-				buttonState = buttonGo;
-				forCnt = 0;
-				bckCnt = 7;
-				cnt = 0;
+			} else if (rotate == 0x01) {
+				route = counterClockwise;
+				phaseCount = (30 / 5.625) * 64;	//Rotate 30 degrees counter clockwise
+				motor = rotate;
+				forwardCount = 0;
+				backwardCount = 7;
+				counter = 0;
 				++i;
-		}
-		break;
-		case buttonGo:
-			if(cnt < maxCnt )
-				buttonState = buttonGo;
-			else{
-				buttonState = buttonWait;
 			}
-		break;
+			break;
+		case rotate:
+			if (counter < phaseCount )
+				motor = rotate;
+			else{
+				motor = wait;
+			}
+			break;
 	}
 
 	// Actions
-	switch(buttonState){
-		case buttonWait:
-		if (i == 18 && button == 0x02) {
-			button = 0x01;
-			i = 0;
-		} else if (i == 18 && button == 0x01) {
-			button = 0x02;
-			i = 0;
-		}
-		break;
-		case buttonGo:
-		if(direction == forward){
-			PORTC = stepOut[forCnt];
-			if(forCnt < 7)
-			forCnt++;
-			else
-			forCnt=0;
-		}
-		else if(direction == backward){
-			PORTC = stepOut[bckCnt];
-			if(bckCnt > 0)
-			bckCnt--;
-			else
-			bckCnt=7;
-		}
-		cnt++;
-		break;
+	switch(motor) {
+		case wait:
+			if (i == 6 && rotate == 0x02) {
+				rotate = 0x01;
+				i = 0;
+			} else if (i == 6 && rotate == 0x01) {
+				rotate = 0x02;
+				i = 0;
+			}
+			break;
+		case rotate:
+			if (route == clockwise) {
+				PORTA = steps[forwardCount];	//Perform the stepper motor pattern
+				if (forwardCount < 7)
+					forwardCount++;
+				else
+					forwardCount=0;
+			} else if (route == counterClockwise) {
+				PORTA = steps[backwardCount];	//Perform the stepper motor pattern
+				if(backwardCount > 0)
+					backwardCount--;
+				else
+					backwardCount=7;
+			}
+			counter++;
+			break;
 	}
 }
 
-//Global variables
-unsigned long timer0Counter = 0x00;
-unsigned char timerOn = 0x01;
-unsigned long distance = 0x00;
-unsigned long input = 0x00;
-unsigned char count = 0x00;
-unsigned char pick = 0x00;
-
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0x00;	//Input from other ATmega1284
-	DDRB = 0xFF; PORTB = 0x00;	//LCD data lines
+	DDRA = 0x00; PORTA = 0x00;  //Input from other ATmega1284
+	DDRB = 0xFF; PORTB = 0x00;  //LCD data lines
 	DDRC = 0xFF; PORTC = 0x00;  //LCD control lines
-	DDRD = 0x12; PORTD = 0x00;	//For Ultrasonic Sensor (ECHO on PD3; TRIG on PD4)
+	DDRD = 0x12; PORTD = 0x00;  //For Ultrasonic Sensor (ECHO on PD3; TRIG on PD4)
 
 	initUSART(0);
 	LCD_init();
 	TimerSet(3);
 	TimerOn();
 	
+	unsigned long timer0Counter = 0x00;
+	unsigned char timerOn = 0x01;
+	unsigned long distance = 0x00;
+	unsigned long input = 0x00;
+	unsigned char count = 0x00;
+	unsigned char pick = 0x00;
+	
 	while (1) {
+		//If 250ms has passed, get distance. Else, rotate stepper motor
 		if (count == 83) {
-			timerOn = 0x01;
-			_delay_us(10);						  //Wait 10 us
-			PORTD = 0x10;						  //Set trig HIGH
-			_delay_us(10);						  //Wait 10 us
-			PORTD = 0x00;						  //Set trig LOW
-			_delay_us(10);						  //Set 10 us
-		
-			EICRA |= (1 << ISC11) | (1 << ISC10); //Trigger on rising edge of INT1
-			EIMSK |= 1 << INT1;					  //Enable interrupts on INT1
-			_delay_ms(10);
-		
-			EIMSK = 0x00;						  //Disable interrupts on INT1
-			EICRA = 0x00;						  //Reset triggering event
-			timerOn = 0x00;						  //Stop timer variable
-			EICRA |= (1 << ISC11) | (0 << ISC10); //Trigger on falling edge of INT1
-			//EIFR |= 1 << INTF1;				  //Reset flag as required by datasheet
-			EIMSK |= 1 << INT1;					  //Enable interrupts on INT1 again
-			//_delay_ms(10);						  //Delay time to "catch" falling edge, else falling edge doesn't get picked up
-			count = 0;
+			getDistance();
 		} else {
 			Tick();
 		}
@@ -152,9 +159,10 @@ ISR(INT1_vect) {
 		TIMSK0 = 0x00;				//Disable overflow interrupts 
 		char* test[10];	
 		char* test2[10];			
-		distance = (timer0Counter * 255) + TCNT0; //Calculate distance
-		distance = distance / 920;				  //Convert to inches (Rough approximation)
+		distance = (timer0Counter * 255) + TCNT0; 		//Calculate distance
+		distance = distance / 920;				//Convert to inches (Rough approximation)
 		
+		/* Begin testing case
 		if (distance <= 60 && distance >= 10) {					  //'if' case is here to avoid bug of distance being over 1000 inches
 			LCD_ClearScreen();
 			ltoa(distance, test, 10);
@@ -175,6 +183,9 @@ ISR(INT1_vect) {
 			//LCD_ClearScreen();
 			LCD_DisplayString(22, "Too close!");
 		}
+		/* End testing case*/
+		
+		input = PINA;
 		
 		//Get choice
 		if (USART_HasTransmitted(0)) {
