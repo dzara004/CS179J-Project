49e8da67adccf7f66c268156ee248d07ba03779e
dzara004 <dzara004@ucr.edu>
2018-05-13 09:47:35 -0700

    Update InsideIntegration.c

--- InsideIntegration.c
+++ InsideIntegration.c
@@ -10,7 +10,8 @@
 #include "usart_ATmega1284.h"
 
 //Global variables for bluetooth communication
-unsigned char select = 0x00;
+unsigned char select = 105;
+unsigned char val = 0x00;
 unsigned char d1 = 0x00;
 unsigned char a1 = 0x00;
 unsigned char d2 = 0x00;
@@ -18,114 +19,150 @@ unsigned char a2 = 0x00;
 char* data[10];
 
 //States for bluetooth communication
-enum BTStates{wait, distance1, angle1, distance2, angle2, receive} BTState;
+enum BTStates{wait, distance1, angle1, distance2, angle2} BTState;
 
 //Bluetooth communication state machine
 void BTTick() {
 	// Transitions
 	switch(BTState) {
 		case wait:
-			if (select == 0) {
-				BTState = wait;
-			} else if (select == 1) {
+			if (select == 101) {
 				BTState = distance1;
-			} else if (select == 2) {
+			} else if (select == 102) {
 				BTState = angle1;
-			} else if (select == 3) {
+			} else if (select == 103) {
 				BTState = distance2;
-			} else if (select == 4) {
+			} else if (select == 104) {
 				BTState = angle2;
+			} else if (select == 105) {
+				BTState = wait;
 			}
 			break;
 		case distance1:
-			BTState = receive;
+			if (select == 101) {
+				BTState = distance1;
+			} else if (select != 101) {
+				BTState = wait;
+			}
 			break;
 		case angle1:
-			BTState = receive;
+			if (select == 102) {
+				BTState = angle1;
+			} else if (select != 102) {
+				BTState = wait;
+			}
 			break;
 		case distance2:
-			BTState = receive;
+			if (select == 103) {
+				BTState = distance2;
+			} else if (select != 103) {
+				BTState = wait;
+			}
 			break;
 		case angle2:
-			BTState = receive;
-			break;
-		case receive:
-			BTState = wait;
+			if (select == 104) {
+				BTState = angle2;
+			} else if (select != 104) {
+				BTState = wait;
+			}
 			break;
 	}
 
 	// Actions
 	switch(BTState) {
 		case wait:
-			++select;
-			if (select == 5) {
-				select = 0;
+			if (USART_HasReceived(0)) {
+				val = USART_Receive(0);
+			}
+			
+			if (val == 101) {
+				select = 101;
+			} else if (val == 102) {
+				select = 102;
+			} else if (val == 103) {
+				select = 103;
+			} else if (val == 104) {
+				select = 104;
+			} else {
+				select = 105;
 			}
 			break;
 		case distance1:
-			if (USART_IsSendReady(0)) {
-				USART_Send(select, 0);
-				while (!(USART_HasTransmitted(0))) {}
+			if (USART_HasReceived(0)) {
+				d1 = USART_Receive(0);
+				USART_Flush(0);
+				select = 105;
 			}
+			itoa(d1, data, 10);
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Distance 1:");
+			LCD_DisplayString(17, data);
 			break;
 		case angle1:
-			if (USART_IsSendReady(0)) {
-				USART_Send(select, 0);
-				while (!(USART_HasTransmitted(0))) {}
+		PORTC = 0x01;
+		_delay_ms(1000);
+		PORTC = 0x00;
+		_delay_ms(1000);
+			if (USART_HasReceived(0)) {
+				a1 = USART_Receive(0);
+				USART_Flush(0);
+				select = 105;
 			}
+			itoa(a1, data, 10);
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Angle 1:");
+			LCD_DisplayString(17, data);
 			break;
 		case distance2:
-			if (USART_IsSendReady(0)) {
-				USART_Send(select, 0);
-				while (!(USART_HasTransmitted(0))) {}
+			if (USART_HasReceived(0)) {
+				d2 = USART_Receive(0);
+				USART_Flush(0);
+				select = 105;
 			}
+			itoa(d2, data, 10);
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Distance 2:");
+			LCD_DisplayString(17, data);
 			break;
 		case angle2:
-			if (USART_IsSendReady(0)) {
-				USART_Send(select, 0);
-				while (!(USART_HasTransmitted(0))) {}
+			if (USART_HasReceived(0)) {
+				a2 = USART_Receive(0);
+				USART_Flush(0);
+				select = 105;
 			}
-			break;
-		case receive:
-			if (select == 1) {
-				if (USART_HasReceived(0)) {
-					d1 = USART_Receive(0);
-					USART_Flush(0);
-				}
-				itoa(d1, data, 10);
-				LCD_ClearScreen();
-				LCD_DisplayString(1, "Distance 1:");
-				LCD_DisplayString(17, data);
-			} else if (select == 2) {
-				if (USART_HasReceived(0)) {
-					a1 = USART_Receive(0);
-					USART_Flush(0);
-				}
-				itoa(a1, data, 10);
-				LCD_ClearScreen();
-				LCD_DisplayString(1, "Angle 1:");
-				LCD_DisplayString(17, data);
-			} else if (select == 3) {
-				if (USART_HasReceived(0)) {
-					d2 = USART_Receive(0);
-					USART_Flush(0);
-				}
-				itoa(d2, data, 10);
-				LCD_ClearScreen();
-				LCD_DisplayString(1, "Distance 2:");
-				LCD_DisplayString(17, data);
-			} else if (select == 4) {
-				if (USART_HasReceived(0)) {
-					a2 = USART_Receive(0);
-					USART_Flush(0);
-				}
-				itoa(a2, data, 10);
-				LCD_ClearScreen();
-				LCD_DisplayString(1, "Angle 2:");
-				LCD_DisplayString(17, data);
-			}
+			itoa(a2, data, 10);
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "Angle 2:");
+			LCD_DisplayString(17, data);
 			break;
 	}
+}
+
+void receiveData() {
+	PORTC = 0x01;
+	_delay_ms(1000);
+	PORTC = 0x00;
+	_delay_ms(1000);
+	
+	if (USART_HasReceived(0)) {
+		d1 = USART_Receive(0);
+		USART_Flush(0);
+	}
+	
+	char* str[10];
+	itoa(d1, str, 10);
+	LCD_ClearScreen();
+	LCD_DisplayString(1, str);
+	
+	if (USART_HasReceived(0)) {
+		d1 = USART_Receive(0);
+		USART_Flush(0);
+	}
+	
+	char* test[10];
+	itoa(d1, test, 10);
+	LCD_ClearScreen();
+	LCD_DisplayString(1, test);
 }
 
 int main(void)
@@ -138,7 +175,8 @@ int main(void)
 	LCD_init();
 	LCD_ClearScreen();
 	initUSART(0);
-	TimerSet(500);
+	//550 okay (only bug is in angle 1)
+	TimerSet(525);
 	TimerOn();
 	
 	BTState = wait;
@@ -146,16 +184,13 @@ int main(void)
 	unsigned char test = 0x00;
 	unsigned char pick = 0x01;
 	char* str[10];
-	
+
 	while (1) {
 		
-		if (USART_HasReceived(0)) {
-			test = USART_Receive(0);
-			USART_Flush(0);
-		}
-		itoa(test, str, 10);
-		LCD_ClearScreen();
-		LCD_DisplayString(1, str);
+		//receiveData();
+		//itoa(d1, str, 10);
+		//LCD_ClearScreen();
+		//LCD_DisplayString(1, str);
 		
 		//Send choice
 		//if (USART_IsSendReady(0)) {
@@ -184,9 +219,10 @@ int main(void)
 		_delay_ms(1000);
 		*/
 		
-		//BTTick();
+		BTTick();
 		
 		while (!TimerFlag) {}
 		TimerFlag = 0;
+		//receiveData();
 	}
 }
