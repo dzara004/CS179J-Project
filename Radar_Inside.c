e8a32d371b0b6938430606e4679c2f59d1c70930
dzara004 <dzara004@ucr.edu>
2018-06-04 20:40:29 -0700

    Update Radar_Inside.c

--- Radar_Inside.c
+++ Radar_Inside.c
@@ -161,39 +161,115 @@ void BTTick() {
 	}
 }
 
+//PWM code given in 120B
+void set_PWM(double frequency) {
+	static double current_frequency; // Keeps track of the currently set frequency
+	// Will only update the registers when the frequency changes, otherwise allows
+	// music to play uninterrupted.
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR0B &= 0x08; } //stops timer/counter
+		else { TCCR0B |= 0x03; } // resumes/continues timer/counter
+		
+		// prevents OCR3A from overflowing, using prescaler 64
+		// 0.954 is smallest frequency that will not result in overflow
+		if (frequency < 0.954) { OCR0A = 0xFFFF; }
+		
+		// prevents OCR0A from underflowing, using prescaler 64					// 31250 is largest frequency that will not result in underflow
+		else if (frequency > 31250) { OCR0A = 0x0000; }
+		
+		// set OCR3A based on desired frequency
+		else { OCR0A = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT0 = 0; // resets counter
+		current_frequency = frequency; // Updates the current frequency
+	}
+}
+
+void PWM_on() {
+	TCCR0A = (1 << COM0A0 | 1 << WGM00);
+	// COM3A0: Toggle PB3 on compare match between counter and OCR0A
+	TCCR0B = (1 << WGM02) | (1 << CS01) | (1 << CS00);
+	// WGM02: When counter (TCNT0) matches OCR0A, reset counter
+	// CS01 & CS30: Set a prescaler of 64
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR0A = 0x00;
+	TCCR0B = 0x00;
+}
+
 int main(void)
 {
 	DDRA = 0xFF; PORTA = 0x00;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-	DDRD = 0xF2; PORTD = 0x00;	//For Ultrasonic Sensor (ECHO on PD3; TRIG on PD4)
+	DDRD = 0xFA; PORTD = 0x00;	//For Ultrasonic Sensor (ECHO on PD3; TRIG on PD4)
 
 	//Initializations
+	PWM_on();
+	//set_PWM(500);
+	//PWM_off();
 	initGraphic();
 	clearScreen();
 	drawAxes();
+	_delay_ms(5000);
 	
 	LCD_init();
 	LCD_ClearScreen();
 	initUSART(0);
+	initUSART(1);
 	//540 fine without PIR motion sensor
 	//600 find with PIR motion sensor
 	//620 is the best by far, only bug occurring every ~5 passes
 	//630 is even better than 620 with one bug occurring every ~20-25 passes
-	TimerSet(630);
+	//250 is testing
+	//189 because 75x2.53
+	TimerSet(129);
 	TimerOn();
 	
 	BTState = wait;
 	
 	double angleOne;
 	double angleTwo;
+	unsigned char plotTimer = 0;
+	unsigned char normalCount = 0;
 	
 	while (1) {
 		BTTick();
 		angleOne = a1 * (M_PI / 180);
 		angleTwo = a2 * (M_PI / 180);
-		plotLeft(d1, angleOne);
-		plotRight(d2, angleTwo);
+		
+		//Integration portion
+		if (USART_IsSendReady(1)) {
+			USART_Send(m, 1);
+			while (!USART_HasTransmitted(1)) {}
+		}
+		
+		//if (((d1 < 12) && (m == 0)) || ((d2 < 12) & (m == 0))) {
+			//++normalCount;
+			//if (normalCount == 10) {
+				//plotLeft(d1, angleOne, m);
+				//plotRight(d2, angleTwo, m);
+				//plotTimer = 0;
+				//normalCount = 0;
+			//}
+		//} else if (plotTimer == 20) {
+			//plotLeft(d1, angleOne, m);
+			//plotRight(d2, angleTwo, m);
+			//plotTimer = 0;
+		//}
+		if (plotTimer == 20) {
+			plotLeft(d1, angleOne, m);
+			plotRight(d2, angleTwo, m);
+			plotTimer = 0;
+		}
+		if (((m != 0) && (d1 < 12)) || ((m != 0) && (d2 < 12))) {
+			set_PWM(500);
+		} else {
+			set_PWM(0);
+		}
+		++plotTimer;
 		while (!TimerFlag) {}
 		TimerFlag = 0;
 	}
